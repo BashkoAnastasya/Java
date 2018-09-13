1458e540708aa4a228f700cde2e7f2b37f145454
INTEXSOFT\anastasiya.bashko <nastinm@mail.ru>
2018-09-13 14:57:52 +0300

    Изменен алгоритм подсчета количества элементов

--- CountSymbol.java
+++ CountSymbol.java
@@ -1,34 +1,20 @@
 package com.intexsoft.bashko.anastasiya;
 
-import java.util.Scanner;
-import java.math.*;
-import java.util.Arrays;
-
 public class CountSymbol {
-	static Scanner scan = new Scanner(System.in);
 
-	public static void main(String[] args) {
-		System.out.println("Enter string");
-		String str = scan.next();	
-		char[] symbol = new char[str.length()+1];
-		int [] count = new int[str.length()+1];
-		int j = 0;		
-		for (int i = 0; i < str.length(); i++) {
-			if (str.charAt(i) != symbol[j]) {
-				j +=  1;
-				symbol[j] = str.charAt(i);
-				count[j] = 1;
+	public static void main(String[] strrgs) {
+		String str = "strstrstrstrstrbbbbbbstrstrstrstrccccccccccd";
+		str = str + " ";
+		String strnew = "";
+		int count = 1;
+		for (int i = 0; i < str.length() - 1; i++) {
+			if (str.charAt(i) != str.charAt(i + 1)) {
+				strnew = strnew + str.charAt(i) + count;
+				count = 1;
 			} else {
-				count[j] += 1;
+				count++;
 			}
 		}
-
-		for (int i = 0; i < count.length; i++) {
-			if (count[i] != 0) {
-				System.out.print(String.valueOf(symbol[i]) + "-" + count[i] + " ");
-			}
-		}
-
+		System.out.println(strnew);
 	}
-
 }
