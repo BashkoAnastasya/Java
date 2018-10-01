f2c2d2ee98610067aea54e44c585bfae25e5af41
INTEXSOFT\anastasiya.bashko <nastinm@mail.ru>
2018-10-01 13:16:53 +0300

    Spring boot

--- ProjectWeb/target/classes/import.sql
+++
@@ -1,5 +0,0 @@
-insert into Supplier (id,moniker,name) values(1,'Barcelona','Barcelona');
-
-insert into Supply (id, supplier_id, code, num) values(1,1,'Lionel Messi', 'Forward');
-insert into Supply (id, supplier_id, code, num) values(2,1,'Andreas Inniesta',  'Midfielder');
-insert into Supply (id, supplier_id, code, num) values(3,1,'Pique', 'Defender');
